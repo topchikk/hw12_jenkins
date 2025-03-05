55c5e72be0449cee61ec0cad626ab87404cfee75
vbiryukov <vladbiryukov0123@gmail.com>
2025-03-06 00:37:22 +0500

    доработал конфтест

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,35 +1,52 @@
 from resource.users import new_user
 from pages.registration_page import RegistrationPage
+import allure
 
 
+@allure.title("Заполнение тренировочной формы DemoQA")
 def test_registration_form():
-    registration_page = RegistrationPage()
-    registration_page.open()
-    registration_page.remove_banner()
+    with allure.step("Открытие формы регистрации"):
+        registration_page = RegistrationPage()
+        registration_page.open()
+        registration_page.remove_banner()
 
     # WHEN
-    registration_page.fill_first_name(new_user.first_name)
-    registration_page.fill_last_name(new_user.last_name)
-    registration_page.fill_email(new_user.user_email)
+    with allure.step("Заполнение поля имя"):
+        registration_page.fill_first_name(new_user.first_name)
+        registration_page.fill_last_name(new_user.last_name)
 
-    registration_page.select_gender(new_user.gender)
+    with allure.step("Заполнение поля email"):
+        registration_page.fill_email(new_user.user_email)
 
-    registration_page.fill_phone(new_user.user_number)
-    registration_page.fill_date_of_birth(new_user.month, new_user.year, new_user.day)
+    with allure.step("Выбор пола"):
+        registration_page.select_gender(new_user.gender)
 
-    registration_page.fill_subject(new_user.subjects)
-    registration_page.select_hobbie(new_user.hobbies)
+    with allure.step("Заполнение поля телефонный номер"):
+        registration_page.fill_phone(new_user.user_number)
 
-    registration_page.upload_picture(new_user.images)
+    with allure.step("Выбор даты рождения"):
+        registration_page.fill_date_of_birth(new_user.month, new_user.year, new_user.day)
 
-    registration_page.fill_address(new_user.current_address)
-    registration_page.fill_state(new_user.state)
-    registration_page.fill_city(new_user.city)
+    with allure.step("Выбор предметов"):
+        registration_page.fill_subject(new_user.subjects)
 
-    registration_page.click_submit()
+    with allure.step("Выбор хобби"):
+        registration_page.select_hobbie(new_user.hobbies)
+
+    with allure.step("Загрузка изображения"):
+        registration_page.upload_picture(new_user.images)
+
+    with allure.step("Заполнение полного адреса"):
+        registration_page.fill_address(new_user.current_address)
+        registration_page.fill_state(new_user.state)
+        registration_page.fill_city(new_user.city)
+
+    with allure.step("Отправка формы регистрации"):
+        registration_page.click_submit()
 
     # THEN
-    registration_page.should_registered_user_info_with(new_user.first_name, new_user.last_name, new_user.user_email,
+    with allure.step("Сравнение отправленных и переданных значений"):
+        registration_page.should_registered_user_info_with(new_user.first_name, new_user.last_name, new_user.user_email,
                                                        new_user.gender, new_user.user_number, new_user.day,
                                                        new_user.month, new_user.year, new_user.subjects,
                                                        new_user.hobbies, new_user.images, new_user.current_address,
